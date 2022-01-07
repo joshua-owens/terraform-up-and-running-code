511ae68a2d27a23e8d81b6fde8258b9dc0f761c0
Josh Owens <me@jowens.dev>
2022-01-06 19:46:25 -0600

    standing up webservers with DB data from remote state

--- src/staging/services/webserver-cluster/main.tf
+++ src/staging/services/webserver-cluster/main.tf
@@ -27,16 +27,22 @@ resource "aws_security_group" "instance" {
   }
 }
 
+data "template_file" "user_data" {
+  template = file("user-data.sh")
+
+  vars = {
+    server_port = var.server_port
+    db_address  = data.terraform_remote_state.db.outputs.address
+    db_port     = data.terraform_remote_state.db.outputs.port
+  }
+}
+
 resource "aws_launch_configuration" "example" {
   image_id        = "ami-0c55b159cbfafe1f0"
   instance_type   = "t2.micro"
   security_groups = [aws_security_group.instance.id]
 
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hello, World" > index.html
-              nohup busybox httpd -f -p ${var.server_port} &
-              EOF
+  user_data = data.template_file.user_data.rendered
 
   lifecycle {
     create_before_destroy = true
@@ -147,6 +153,8 @@ data "terraform_remote_state" "db" {
 
 terraform {
   backend "s3" {
-    key = "staging/webserver-cluster/terraform.tfstate"
+    bucket = "jowens-terraform-up-and-running-state"
+    region = "us-east-2"
+    key    = "staging/webserver-cluster/terraform.tfstate"
   }
 }
--- src/staging/services/webserver-cluster/variables.tf
+++ src/staging/services/webserver-cluster/variables.tf
@@ -1,5 +1,5 @@
 variable "bucket_name" {
   default     = "jowens-terraform-up-and-running-state"
   description = "The S3 bucket name to store state file in."
-  value       = string
+  type        = string
 }
