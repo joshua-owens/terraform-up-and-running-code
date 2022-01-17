3fa817fa1b462e300efa47aba03aca2b3e004da4
Josh Owens <me@jowens.dev>
2022-01-17 15:10:56 -0600

    separating out inline security group rules

--- src/modules/services/webserver-cluster/main.tf
+++ src/modules/services/webserver-cluster/main.tf
@@ -12,22 +12,29 @@ variable "server_port" {
   default     = 8080
 }
 
+resource "aws_security_group_rule" "allow_http_inbound" {
+  type              = "ingress"
+  security_group_id = aws_security_group.alb.id
+
+  from_port   = var.server_port
+  protocol    = local.tcp_protocol
+  to_port     = var.server_port
+  cidr_blocks = local.all_ips
+}
+
+resource "aws_security_group_rule" "allow_http_outbound" {
+  type              = "egress"
+  security_group_id = aws_security_group.alb.id
+
+  from_port   = local.any_port
+  to_port     = local.http_port
+  protocol    = local.any_protocol
+  cidr_blocks = local.all_ips
+}
+
 resource "aws_security_group" "instance" {
   name = "${var.cluster_name}-instance"
 
-  ingress {
-    from_port   = var.server_port
-    protocol    = local.tcp_protocol
-    to_port     = var.server_port
-    cidr_blocks = local.all_ips
-  }
-
-  egress {
-    from_port   = local.any_port
-    to_port     = local.http_port
-    protocol    = local.any_protocol
-    cidr_blocks = local.all_ips
-  }
 }
 
 data "template_file" "user_data" {
--- src/modules/services/webserver-cluster/outputs.tf
+++ src/modules/services/webserver-cluster/outputs.tf
@@ -1,8 +1,3 @@
-output "alb_dns_name" {
-  value       = aws_lb.example.dns_name
-  description = "The domain name of the load balancer"
-}
-
 output "asg_name" {
   value       = aws_autoscaling_group.example.name
   description = "The name of the Auto Scaling Group"
@@ -11,4 +6,9 @@ output "asg_name" {
 output "alb_dns_name" {
   value       = aws_lb.example.dns_name
   description = "The domain name of the load balancer"
+}
+
+output "alb_security_group_id" {
+  value       = aws_security_group.alb.id
+  description = module.webserver_cluster.alb_security_group_id
 }
