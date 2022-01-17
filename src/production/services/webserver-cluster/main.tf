13cf11e82a1d7fdb35f96037ec1c72d5afd48911
Josh Owens <me@jowens.dev>
2022-01-16 22:19:49 -0600

    setting up prod schduler

--- src/production/services/webserver-cluster/main.tf
+++ src/production/services/webserver-cluster/main.tf
@@ -2,6 +2,22 @@ provider "aws" {
   region = "us-east-2"
 }
 
+resource "aws_autoscaling_schedule" "scale_out_during_business_hours" {
+  scheduled_action_name = "scale-out-during-business-hours"
+  min_size              = 2
+  max_size              = 10
+  desired_capacity      = 10
+  recurrence            = "0 9 * * *"
+}
+
+resource "aws_autoscaling_schedule" "scale_in_at_night" {
+  scheduled_action_name = "scale-in-at-night"
+  min_size              = 2
+  max_size              = 10
+  desired_capacity      = 2
+  recurrence            = "0 17 * * *"
+}
+
 module "webserver_cluster" {
   source = "../../../modules/services/webserver-cluster"
 
